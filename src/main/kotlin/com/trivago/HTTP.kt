fda7ae4ddd114f269595c79da2282df75437f2a6
kalinin-ng <KalininNG@intech.rshb.ru>
2023-08-17 13:42:41 +0300

    FIxed validation

--- src/main/kotlin/com/trivago/HTTP.kt
+++ src/main/kotlin/com/trivago/HTTP.kt
@@ -1,4 +1,4 @@
-package com.trivago.plugins
+package com.trivago
 
 import com.trivago.accomodation.AccommodationDTO
 import io.ktor.http.*
@@ -14,7 +14,8 @@ import kotlin.math.abs
 import kotlin.math.log10
 
 private val hotelNameForbiddenWordsPattern = "( (free|website|book|offer))|((free|website|book|offer) )".toRegex()
-private val urlPattern = "^https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)$".toRegex()
+private val urlPattern =
+    "^https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)$".toRegex()
 
 fun Application.configureHTTP() {
     install(ContentNegotiation) {
@@ -31,20 +32,19 @@ fun Application.configureHTTP() {
     install(RequestValidation) {
         validate(AccommodationDTO::class) {
             if (it.name.lowercase().contains(hotelNameForbiddenWordsPattern)) {
-              return@validate ValidationResult.Invalid("A hotel name cannot contain the words [\"Free\", \"Offer\", \"Book\", \"Website\"]")
+                return@validate ValidationResult.Invalid("A hotel name cannot contain the words [\"Free\", \"Offer\", \"Book\", \"Website\"]")
             }
             if (it.name.length <= 10) {
-             return@validate   ValidationResult.Invalid("A hotel name should be more than 10 characters")
+                return@validate ValidationResult.Invalid("A hotel name should be more than 10 characters")
             }
             if (it.rating > 5 || it.rating < 0) {
-               return@validate ValidationResult.Invalid("Rating should be between 0 and 5")
+                return@validate ValidationResult.Invalid("Rating should be between 0 and 5")
             }
             if (!listOf("hotel", "alternative", "hostel", "lodge", "resort", "guesthouse").contains(it.category)) {
                 return@validate ValidationResult.Invalid("The category can be one of [hotel, alternative, hostel, lodge, resort, guesthouse]")
             }
-            val isNewLocation = it.location.id == 0L
             val zipDigitsNumber = log10(abs(it.location.zipCode.toDouble())).toInt() + 1
-            if (isNewLocation && zipDigitsNumber != 5) {
+            if (zipDigitsNumber != 5) {
                 return@validate ValidationResult.Invalid("Zip Code should be 5 digits")
             }
             if (!urlPattern.matches(it.image)) {
